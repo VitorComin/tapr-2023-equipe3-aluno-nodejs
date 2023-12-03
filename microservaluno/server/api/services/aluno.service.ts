cf5076758ba76410fbccb6ca559c98b06e6abb77
Vitor Gabriel Comin <106283734+VitorComin@users.noreply.github.com>
2023-12-03 20:22:56 +0000

    Fifth Commit

--- microservaluno/server/api/services/aluno.service.ts
+++ microservaluno/server/api/services/aluno.service.ts
@@ -1,6 +1,8 @@
 import { Container, SqlQuerySpec } from "@azure/cosmos";
 import cosmosDb from "../../common/cosmosdb";
 import { Aluno } from "server/interfaces/aluno";
+import { DaprClient } from "@dapr/dapr";
+import daprClient from "../../common/daprclient";
 
 class AlunoService{
     private container:Container = 
@@ -30,6 +32,7 @@ class AlunoService{
     async saveNew(aluno: Aluno): Promise<Aluno> {
         aluno.id = "";
         await this.container.items.create(aluno);
+        await this.publishEvent(aluno);
 
         return Promise.resolve(aluno);
     }
@@ -47,31 +50,46 @@ class AlunoService{
 
         const alunoAntigo = listaAluno[0];
 
+        if(alunoAntigo == undefined){
+            return Promise.reject();
+        }
+
         alunoAntigo.matricula = aluno.matricula;
         alunoAntigo.nome = aluno.nome;
         alunoAntigo.curso = aluno.curso;
         alunoAntigo.ativo = aluno.ativo;
         await this.container.items.upsert(alunoAntigo)
+        await this.publishEvent(alunoAntigo);
 
         return Promise.resolve(alunoAntigo);
     }
 
-    async delete(id: string): Promise<string> {
+        async delete(id:string): Promise<string>{
+
         const querySpec: SqlQuerySpec = {
             query: "SELECT * FROM Aluno c WHERE c.id = @id",
             parameters: [
-                { name: "@id", value: id }
+                {name: "@id", value: id}
             ]
-        };
-
-        const { resources: listaAluno }
+            };
+        const {resources: listaAluno}
             = await this.container.items.query(querySpec).fetchAll();
-            
         for (const aluno of listaAluno) {
-            await this.container.item(aluno.id, aluno.matricula).delete();
+            await this.container.item(aluno.id).delete();
         }
-
+        
         return Promise.resolve(id);
+
+    
+        
+    }
+
+    async publishEvent(aluno:Aluno): Promise<Aluno>{
+        daprClient.pubsub.publish(process.env.APPCOMPONENTSERVICE as string,
+                                  process.env.APPCOMPONENTTOPICALUNO as string,
+                                  aluno);
+        return Promise.resolve(aluno);
+
     }
 }
 
